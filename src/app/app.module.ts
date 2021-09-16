95da46f1a4443cad6e15b01433a7b63c2983def7
mdupouy2021 <michel.dupouy2021@campus-eni.fr>
2021-09-16 11:47:49 +0200

    oui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitreComponent } from './Composants/titre/titre.component';
+import { PersonnesComponent } from './Composants/personnes/personnes.component';
+import { MessageComponent } from './Composants/message/message.component';
+import { NouveauComponent } from './Composants/nouveau/nouveau.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitreComponent,
+    PersonnesComponent,
+    MessageComponent,
+    NouveauComponent
   ],
   imports: [
     BrowserModule
